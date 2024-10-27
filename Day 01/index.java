a92c7ca0e4be4a0024a408fe4342c349eb2b6808
UsminDhakal <usmindhakal123456789@gmail.com>
2024-10-28 00:27:02 +0545

    Day 1

---
+++ Day 01/index.java
@@ -0,0 +1,9 @@
+public class index {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a = 10; 
+        char c = 'A';
+        System.out.println(c +"Hello");
+        System.out.println("Hello WOrld");
+    }
+}
