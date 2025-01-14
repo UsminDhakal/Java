c6615c8bcdd6338b34846048e5a2cfca6d55d081
UsminDhakal <usmindhakal123456789@gmail.com>
2025-01-14 09:33:50 +0545

    Day 2

---
+++ Day 02/datatypes.java
@@ -0,0 +1,16 @@
+public class datatypes{
+    public static void main(String[] args) {
+        // int a = 10;
+        // System.out.println(a + "Hello");
+        // char b = 'U';
+        // System.out.println(b);  
+
+        byte age = 10;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 10.9d;
+        System.out.println(d1);
+        String a = "Usmin";
+        System.out.println(a);
+    }
+}
---
+++ Day 02/userInput.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class userInput {
+
+    public static void main(String[] args) {
+        System.out.println("Taking Input from scanner");
+       
+        Scanner input = new Scanner(System.in);
+       
+        // System.out.println("Enter number 1");
+        // int a = input.nextInt();
+
+        // System.out.println("ENter number 2");
+        // int b = input.nextInt();
+        // boolean b1 = input.hasNextInt();
+        // System.out.println(b1);
+        // int sum = a+b;
+        // System.out.println("Sum = "+sum);
+        String str1 = input.nextLine();
+        System.out.println("Str1 = " + str1);
+
+
+        // float f1 = input.nextFloat();
+
+
+
+    }
+}
