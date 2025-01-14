a52792ac49aa2ffce2cec206faf1be1805cfa417
UsminDhakal <usmindhakal123456789@gmail.com>
2025-01-14 10:08:44 +0545

    Day 3 completed

--- Day 03/stringsmethods.java
+++ Day 03/stringsmethods.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
+
 public class stringsmethods{
     public static void main(String[] args) {
-        String a = "Usmin";
-        System.out.println(a.length());
-        System.out.println(a.toLowerCase());
-        String b = a.substring(0, 3);
-        System.out.println(b);
-        String c = a.trim();
-        System.out.println(c);
+        String a = "Usminnnn";
+        // System.out.println(a.length());
+        // System.out.println(a.toLowerCase());
+        // String b = a.substring(0, 3);
+        // System.out.println(b);
+        // String c = a.trim();
+        // System.out.println(c);
+        // System.out.println("Hello");
+        // String name = a.toLowerCase();
+        // System.out.println(name);
+        // Scanner input = new Scanner(System.in); 
+        // String usmin = input.nextLine();
+        // String trimmedString = usmin.trim();
+        // System.out.println(trimmedString);
+
+        System.out.println(a.replace("n", "a"));
+        System.out.println(a.startsWith("Usm"));
+
 
     }
 }
