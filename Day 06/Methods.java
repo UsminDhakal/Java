0eedce96df3bc6ac7c175928f9ebc17292c7c760
UsminDhakal <usmindhakal123456789@gmail.com>
2025-02-27 23:19:37 +0545

    Day 06

---
+++ Day 06/Methods.java
@@ -0,0 +1,14 @@
+
+public class Methods {
+
+    static int multiply(int x, int y){
+        return (x+y)*5;
+    }
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c = multiply(a, b);
+        System.out.println(c);
+        
+    }
+}
