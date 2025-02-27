87a8aa0c196654d38a05e77e405a49e5148e4c78
UsminDhakal <usmindhakal123456789@gmail.com>
2025-02-27 23:34:30 +0545

    CHanges:

--- Day 06/Methods.java
+++ Day 06/Methods.java
@@ -1,14 +1,24 @@
 
 public class Methods {
 
-    static int multiply(int x, int y){
+    // static int multiply(int x, int y){
+    //     return (x+y)*5;
+    // }
+
+    int multiply(int x, int y){
         return (x+y)*5;
     }
     public static void main(String[] args) {
         int a = 5;
         int b = 7;
-        int c = multiply(a, b);
-        System.out.println(c);
+        // int c = multiply(a, b);
+        // System.out.println(c);
         
+        Methods obj = new Methods();
+        int c;
+        c = obj.multiply(5, 6);
+        System.out.println(c);
+
+
     }
 }
