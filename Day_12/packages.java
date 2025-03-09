d32d44b86a7145e4df7ebacf8a01fcea19f377cc
UsminDhakal <usmindhakal123456789@gmail.com>
2025-03-09 09:49:29 +0545

    More about pacakages

---
+++ Day_12/packages.java
@@ -0,0 +1,7 @@
+package learningpackages;
+
+public class packages {
+    public static void main(String[] args) {
+        System.out.println("Hello My name is Usmin Dhakal.");
+    }
+}
---
+++ Day_12/usmingpackages.java
@@ -0,0 +1,10 @@
+// package Day_12;
+import learningpackages.packages;
+
+
+public class usmingpackages {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        packages g = new packages();
+    }
+}
