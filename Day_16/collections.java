7d18477f9545903367920048e2c88cdbb46b52c9
UsminDhakal <usmindhakal123456789@gmail.com>
2025-03-13 19:10:00 +0545

    Collection

---
+++ Day_16/collections.java
@@ -0,0 +1,15 @@
+package Day_16;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class collections {
+    public static void main(String[] args) {
+        // ArrayList
+        // Set
+        // TreeSet
+
+        //Example of collections
+    }
+}
