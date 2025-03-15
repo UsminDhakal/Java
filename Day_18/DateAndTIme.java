efd6bb346e13bf9c9aa3995b630ebf6addd5ebb7
UsminDhakal <usmindhakal123456789@gmail.com>
2025-03-15 18:31:48 +0545

    Date And TIme

---
+++ Day_18/DateAndTIme.java
@@ -0,0 +1,12 @@
+// import java.time.LocalDate;
+// import java.time.LocalDateTime;
+
+public class DateAndTIme {
+    public static void main(String[] args) {
+        // LocalDate obj = LocalDate.now();
+        // System.out.println(obj);
+
+        System.out.println(System.currentTimeMillis());
+    }
+    
+}
